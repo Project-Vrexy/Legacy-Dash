c4d5c1747b501dffc0a1871f3f53c87f26d05d85
Sadie K. Brooklyn <senkodev@gmail.com>
2021-12-15 19:24:39 +0300

    no more legacy dashboard edit

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from dotenv import load_dotenv
 
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
-load_dotenv('.env')
+load_dotenv()
 app = Flask(__name__)
 app.secret_key = os.getenv("FLASK_DANCE_SECRET")
 
@@ -59,7 +59,7 @@ def servers_list():
     if not discord.authorized:
         return redirect(url_for("discord.login"))
 
-    con = sqlite3.connect(f'{os.getenv("BOT_DIR")}db/database.sqlite3')
+    con = sqlite3.connect(os.getenv('DB_PATH'))
     cur = con.cursor()
 
     user = discord.get('https://discord.com/api/v7/users/@me')
@@ -69,7 +69,7 @@ def servers_list():
     userd['gif'] = True if userd['avatar'].endswith("_a") else False
 
     with con:
-        cur.execute('SELECT id FROM config')
+        cur.execute('SELECT * FROM internal')
         current_aeon_guilds = cur.fetchall()
 
     ids = [row[0] for row in current_aeon_guilds]
@@ -95,7 +95,7 @@ def servers_list():
 
             list.append(gdict)
 
-        if guild['permissions'] == 8 or guild['owner'] is True:
+        if (int(guild["permissions"]) & 0x8) == 0x8 or guild['owner'] is True:
             if int(guild['id']) in ids:
                 append(servers)
             else:
@@ -125,11 +125,11 @@ def edit_guild():  # sourcery no-metrics skip
             flash("You need to provide a server ID.", "danger")
             return redirect(url_for("servers_list"))
 
-    con = sqlite3.connect(f'{os.getenv("BOT_DIR")}db/database.sqlite3')
+    con = sqlite3.connect(os.getenv('DB_PATH'))
     cur = con.cursor()
 
     with con:
-        cur.execute('SELECT * FROM config WHERE id = :id', {'id': guild})
+        cur.execute('SELECT * FROM internal WHERE guild = :id', {'id': guild})
         tuple = cur.fetchone()
 
         if not tuple:
@@ -139,14 +139,10 @@ def edit_guild():  # sourcery no-metrics skip
         config = [item for item in tuple]
         config.pop(0)
 
-        pwc = True if config[6] == "ON" else False
-        atc = True if config[0] == "ON" else False
-        awc = True if config[3] == "ON" else False
-
     guilds = discord.get('https://discord.com/api/v7/users/@me/guilds')
 
     with con:
-        cur.execute('SELECT id FROM config')
+        cur.execute('SELECT * FROM internal')
         current_aeon_guilds = cur.fetchall()
 
     ids = [row[0] for row in current_aeon_guilds]
@@ -166,7 +162,7 @@ def edit_guild():  # sourcery no-metrics skip
                 }
             )
 
-        if _guild['permissions'] == 8 or _guild['owner'] is True:
+        if (int(_guild["permissions"]) & 0x8) == 0x8 or _guild['owner'] is True:
             if int(_guild['id']) in ids:
                 append(servers)
                 server_ids.append(_guild['id'])
@@ -189,75 +185,9 @@ def edit_guild():  # sourcery no-metrics skip
             currguild.append(server)
 
     if request.method == "POST":
-        prefix = request.form["pfx"]
-        pfx_warn = 'on' if 'pfx-warn' in request.form else 'off'
-
-        as_toggle = 'on' if 'as-toggle' in request.form else 'off'
-        as_words = request.form['as-words'] if 'as-words' in request.form else config[1]
-        as_wc = 'on' if "as-wc" in request.form else 'off'
-
-        if prefix:
-            prefix = prefix.replace('"', "").replace("{s}", " ").replace(
-                "`", "").replace("﷽", "").replace("'", "")
-
-            if prefix in ["", " "]:
-                flash("That prefix contains an invalid character.", "danger")
-                return render_template('dashboard/form.html', config=config, currguild=currguild[0], pwc=pwc, atc=atc, awc=awc)
-
-            with con:
-                cur.execute('UPDATE config SET prefix = :prefix WHERE id = :id', {
-                    'id': guild,
-                    'prefix': prefix
-                })
-
-        if pfx_warn:
-            with con:
-                cur.execute('UPDATE config SET prefix_warn = :pw WHERE id = :id', {
-                    'id': guild,
-                    'pw': pfx_warn.upper()
-                })
-
-        if as_toggle:
-            with con:
-                cur.execute('UPDATE config SET delete_swear = :pw WHERE id = :id', {
-                    'id': guild,
-                    'pw': as_toggle.upper()
-                })
-
-        if as_words:
-            with con:
-                if as_words == "reset":
-                    cur.execute('UPDATE config SET swears = :swears WHERE :id = id', {
-                        'id': guild,
-                        'swears': None
-                    })
-                else:
-                    cur.execute('UPDATE config SET swears = :swears WHERE :id = id', {
-                        'id': guild,
-                        'swears': as_words.replace(" ", ", ")
-                    })
-
-        if as_wc:
-            with con:
-                cur.execute('UPDATE config SET wildcard = :as_wc WHERE :id = id', {
-                    'id': guild,
-                    'as_wc': as_wc.upper()
-                })
-
-        with con:
-            cur.execute('SELECT * FROM config WHERE id = :id', {'id': guild})
-            tuple = cur.fetchone()
-
-            config = [item for item in tuple]
-            config.pop(0)
-
-            pwc = True if config[6] == "ON" else False
-            atc = True if config[0] == "ON" else False
-            awc = True if config[3] == "ON" else False
-
         flash("All Changes Saved.", "success")
 
-    return render_template('dashboard/form.html', config=config, currguild=currguild[0], pwc=pwc, atc=atc, awc=awc)
+    return render_template('dashboard/form.html', currguild=currguild[0])
 
 
 @app.route("/dashboard/logout/")
